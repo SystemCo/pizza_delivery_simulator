3600218f90c1ed2bcea80d53cf85ecb2e976a4a3
TrashChan1 <evenduregalendale@gmail.com>
2025-02-11 09:12:25 -0800

    added pedal and motorcycle data abstractions to my personal source file

--- david.cpp
+++ david.cpp
@@ -2,6 +2,16 @@
 // Purpose: My personal contributions to the project
 // Date: Spring 2025
 
+enum Pedal { Forward, Neutral, Backward };
+
+struct motorcycle {
+    int x;
+    int y;
+    int rotation;
+    Pedal pedal;
+
+};
+
 double accelerate(double velocity, int forward)
 {
     return velocity + forward;

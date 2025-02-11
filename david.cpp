ef71a100b0baef1d797bb9bff72505160d75e074
TrashChan1 <evenduregalendale@gmail.com>
2025-02-11 09:03:19 -0800

    Added my personal source file

---
+++ david.cpp
@@ -0,0 +1,9 @@
+// Author: David Carter
+// Purpose: My personal contributions to the project
+// Date: Spring 2025
+
+double accelerate(double velocity, int forward)
+{
+    return velocity + forward;
+}
+

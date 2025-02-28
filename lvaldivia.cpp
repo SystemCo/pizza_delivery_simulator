6533f2e0755126e922792765692e9074ccdaaa2c
Lesslie Valdivia <lvaldivia@odin.cs.csubak.edu>
2025-02-28 09:43:04 -0800

    added myself to credits

--- lvaldivia.cpp
+++ lvaldivia.cpp
@@ -1,1 +1,13 @@
+//Lesslie Valdivia
+//
+#include "lvaldivia.h"
+#include <iostream>
 
+
+void show_lesslie(Rect* r);
+
+void show_lesslie(Rect* r)
+{
+    ggprint8b(r, 16, 0xFFFF0000, "Lesslie");
+
+}
---
+++ lvaldivia.h
@@ -0,0 +1,8 @@
+#ifndef _LVALDIVIA_H_
+#define _LVALDIVIA_H_
+
+#include "fonts.h"
+
+void show_lesslie(Rect* r);
+
+#endif

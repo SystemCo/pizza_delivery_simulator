778dbce7f54034f5969aec61514438370cd4c38a
Fenoon Alrowhani <falrowhani@odin.cs.csubak.edu>
2025-03-02 14:13:40 -0800

    working on physics

--- dcarter.cpp
+++ dcarter.cpp
@@ -242,19 +242,7 @@ void title_moto_physics(int frame, Animation animation[5])
         *pos_y -= gl.yres;
 }
 
-void title_physics()
-{
-    static int frame = 0; 
-    Animation  animations[5];
-    animations[0] = {5, 0, 0, 0};
-    animations[1] = {5, 0, 1, 0};
-    for (int i = 0; i<5; i++)
-        animations[i] = {1, 1, 0, 0};
-    title_moto_physics(frame, animations);
 
-    frame += 1;
-    frame %= 500;
-}
 
 void title_render()
 {
--- falrowhani.cpp
+++ falrowhani.cpp
@@ -1,3 +1,25 @@
 //Name:Fenoon
 //lab4
 //working on animation
+//
+//original code from Dcarter 
+//modified by me 
+//
+#include "dcarter.h"
+
+void title_moto_physics(int frame, Animation animation[5]);
+
+
+void title_physics()
+{
+    static int frame = 0;
+    Animation  animations[5];
+    animations[0] = {5, 0, 0, 0};
+    animations[1] = {5, 0, 1, 0};
+    for (int i = 0; i<5; i++)
+        animations[i] = {1, 1, 0, 0};
+    title_moto_physics(frame, animations);
+
+    frame += 1;
+    frame %= 500;
+}

28514fee20a7478e8d7c2c2c09ed70102f9da24d
Fenoon Alrowhani <falrowhani@odin.cs.csubak.edu>
2025-03-11 07:27:50 -0700

    name to credits and motorcyle function

--- falrowhani.cpp
+++ falrowhani.cpp
@@ -5,12 +5,13 @@
 //original code from Dcarter 
 //modified by me 
 //
+//#include "GL.h"
 #include "dcarter.h"
 #include <iostream>
 #include "falrowhani.h"
-
+#include "shared.h" 
 //function prototypes 
-void title_moto_physics(int frame, Animation animation[3]);
+//void title_moto_physics(int frame, Animation animation[3]);
 void show_fenoon(Rect*r);
 
 void show_fenoon(Rect*r)
@@ -18,6 +19,7 @@ void show_fenoon(Rect*r)
     ggprint8b(r, 16, 0x00FFFF, "Fenoon");
 }
 
+extern Global gl; 
 
 // I added this prototype to my header file -- David
 //void title_moto_physics(int frame, Animation animation[5]);
@@ -30,10 +32,15 @@ void title_physics()
     animations[1] = {2, 5, 25, 0};
     animations[2] = {2, 0, 0, 0};
 
-    title_moto_physics(frame, animations);
+   // title_moto_physics(frame, animations);
+    
+    const int totalFrames = 500; 
+    const int section_count = 3;     
 
+    // calling animate function direcly 
+    gl.moto_side->animate(frame, animations, section_count, totalFrames);
     frame += 1;
-    frame %= 500;
+    frame %= totalFrames;
 
 }
 
--- falrowhani.h
+++ falrowhani.h
@@ -1,8 +1,19 @@
 #ifndef _FENOON_H_
 #define _FENOON_H_
+//#ifndef GL_H
+//#define GL_H
+
 
 #include "fonts.h"
 
+
+//class GL {
+//public:
+  //  SomeType* moto_side;
+
+//};
+
+//extern GL gl; 
 void show_fenoon(Rect*r);
 
 #endif
